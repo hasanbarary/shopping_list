16982f6e7f5a1021f952e1d68596926ab423e631
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-05 23:33:25 +0330

    Update shopping_list.py
    edit page

--- shopping_list.py
+++ shopping_list.py
@@ -1,10 +1,12 @@
 #!/bin/python3.4
+
 #Help Function
 def print_help():
     print("What should we use at the store ?\n")
     print("Enter 'DONE' to stop adding items")
     print("Enter 'HELP' to show all instruction in our app ")
     print("Enter 'SHOW' to show all items in our shopping list")
+
 #Apend Function 
 def append_list(shopping_list,answer):
     if answer not in shopping_list:
