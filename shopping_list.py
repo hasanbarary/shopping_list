605b1c0d42d7dbc8643efd5440dc1d5fa774d64a
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-05 23:32:50 +0330

    Update shopping_list.py
    Add Apend Function to Main File

--- shopping_list.py
+++ shopping_list.py
@@ -1,10 +1,19 @@
 #!/bin/python3.4
+#Help Function
 def print_help():
     print("What should we use at the store ?\n")
     print("Enter 'DONE' to stop adding items")
     print("Enter 'HELP' to show all instruction in our app ")
     print("Enter 'SHOW' to show all items in our shopping list")
-
+#Apend Function 
+def append_list(shopping_list,answer):
+    if answer not in shopping_list:
+        shopping_list.append(answer)
+        "{} Added Into List ".format(answer)
+    else:
+        "{} repeated".format(answer)
+        
+#main
 shopping_list=[]
 print_help()
 while True:
