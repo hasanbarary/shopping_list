99e106dc66a2fe5eacab9805c29ef4592433bf04
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-05 23:30:52 +0330

    Update shopping_list.py
    add help function on base File

--- shopping_list.py
+++ shopping_list.py
@@ -1,8 +1,12 @@
 #!/bin/python3.4
+def print_help():
+    print("What should we use at the store ?\n")
+    print("Enter 'DONE' to stop adding items")
+    print("Enter 'HELP' to show all instruction in our app ")
+    print("Enter 'SHOW' to show all items in our shopping list")
 
 shopping_list=[]
 print_help()
-#print('test')
 while True:
     answer = input().lower()
     if answer == 'help':
