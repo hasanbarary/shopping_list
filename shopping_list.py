ec024bfcf24c27ed870bd035f6c6e3790e8b7bae
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-06 07:02:18 +0300

    Update shopping_list.py
    add remove function and reform code

--- shopping_list.py
+++ shopping_list.py
@@ -6,6 +6,7 @@ def menu():
     print("Enter 'DONE' to stop adding items")
     print("Enter 'HELP' to show all instruction in our app ")
     print("Enter 'SHOW' to show all items in our shopping list")
+    print("Enter 'REMOVE' to remove item  in our shopping list\n")
 #Apend Function 
 def append_list(items,answer):
     if answer not in items:
@@ -13,16 +14,27 @@ def append_list(items,answer):
         print("you added {} into your list. Now you have {} items".format(answer,len(items)))
     else:
         print("{} repeated".format(answer))
+def remove_item(items,answer):
+        if answer in items:
+            items.remove(answer)
+            print("{} deleted from your shopping list ".format(answer))
+        else:
+            print("{} not exist on shopping list".format(answer))
 #main
 items=[]
 menu()
 while True:
-    answer = input().lower()
+    answer = input("Choose From Menu -->").lower()
     if answer == 'help':
         menu()
     elif answer == 'done':
         break
     elif answer == 'show':
         print(items)
+    elif answer == 'remove':
+        answer = input("Enter your Item For Delete -->  ").lower()
+        remove_item(items,answer)
+    elif answer == '':
+        continue
     else:
         append_list(items,answer)
